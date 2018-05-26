1097cd29e773def4c901ff91489eb5be3feccf46
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-26 16:59:37 +0100

    CTA ends

--- src/App.js
+++ src/App.js
@@ -190,6 +190,15 @@ class App extends Component {
                 <button className="btn-inline">Show all <span>&rarr;</span></button>
               </div>
             </div>
+            <div className="cta">
+              <h2 className="cta__book-now">
+                Good news! We have 4 free rooms for your selected dates!
+                </h2>
+              <button className="btn">
+                <span className="btn__visible">Book now</span>
+                <span className="btn__invisible">Only 4 rooms left</span>
+              </button>
+            </div>
           </main>
         </div>
       </div >
