4d1a8089f5dc7833a65bccf3b1cab34db6b67928
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-05-26 09:06:08 +0100

    - Using` background-image` to add SVG with CSS. - also the use of `masks` on newer browsers

--- src/App.js
+++ src/App.js
@@ -153,12 +153,12 @@ class App extends Component {
                   <p className="recommend__count">
                     Lucy and 3 other friends recommend this hotel.
                   </p>
-                </div>
-                <div className="recommend__friends">
-                  <img src="img/user-3.jpg" alt="Friend 1" className="recommend__photo" />
-                  <img src="img/user-4.jpg" alt="Friend 2" className="recommend__photo" />
-                  <img src="img/user-5.jpg" alt="Friend 3" className="recommend__photo" />
-                  <img src="img/user-6.jpg" alt="Friend 4" className="recommend__photo" />
+                  <div className="recommend__friends">
+                    <img src="img/user-3.jpg" alt="Friend 1" className="recommend__photo" />
+                    <img src="img/user-4.jpg" alt="Friend 2" className="recommend__photo" />
+                    <img src="img/user-5.jpg" alt="Friend 3" className="recommend__photo" />
+                    <img src="img/user-6.jpg" alt="Friend 4" className="recommend__photo" />
+                  </div>
                 </div>
               </div>
               <div className="user-reviews">
